17a4446acca70b71f7dfa77d9d63b82f577032e1
Mo Morsi <mmorsi@redhat.com>
2011-07-29 01:31:31 -0400

    initial redmine plugin

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_patch_mine do
+  name 'Redmine PatchMine plugin'
+  author 'Mo Morsi'
+  description 'A plugin for extended Patch integration into redmine'
+  version '0.0.1'
+  url 'http://github.com/movitto/patchmine'
+  author_url 'http://mo.morsi.org'
+end
